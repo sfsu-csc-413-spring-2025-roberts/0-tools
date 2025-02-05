d10a6be07f53971ed9d4b0a7a8364403958efc47
John Roberts <roberts.john@gmail.com>
2025-02-04 19:24:24 -0800

    starting on CompilerTools

--- src/tools/CompilerTools.java
+++ src/tools/CompilerTools.java
@@ -1,9 +1,5 @@
 package tools;
 
-import java.nio.file.Path;
-
-import tools.readers.GrammarReader;
-
 public class CompilerTools {
     public static void main(String[] args) {
         if (args.length != 2) {
@@ -11,6 +7,6 @@ public class CompilerTools {
             System.exit(1);
         }
 
-        GrammarReader reader = new GrammarReader(Path.of(args[0]), Path.of(args[1]));
+        // GrammarReader reader = new GrammarReader(Path.of(args[0]), Path.of(args[1]));
     }
 }
