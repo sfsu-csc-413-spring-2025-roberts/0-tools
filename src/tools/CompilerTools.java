aa38de127eba692acabded22301e6bc2c7a6078c
John Roberts <roberts.john@gmail.com>
2025-02-04 19:23:52 -0800

    Implemented the ToolFileReader base class

--- src/tools/CompilerTools.java
+++ src/tools/CompilerTools.java
@@ -1,5 +1,16 @@
 package tools;
 
-public class CompilerTools {
+import java.nio.file.Path;
 
+import tools.readers.GrammarReader;
+
+public class CompilerTools {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java CompilerTools <constants-file> <grammar-file>");
+            System.exit(1);
+        }
+
+        GrammarReader reader = new GrammarReader(Path.of(args[0]), Path.of(args[1]));
+    }
 }
--- src/tools/readers/ToolFileReader.java
+++ src/tools/readers/ToolFileReader.java
@@ -1,5 +1,32 @@
 package tools.readers;
 
-public class ToolFileReader {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.List;
 
+public abstract class ToolFileReader implements Iterator<String> {
+    private int index;
+    private List<String> lines;
+
+    public ToolFileReader(Path path) throws IOException {
+        this.index = 0;
+
+        this.lines = Files.readAllLines(path).stream()
+                .filter(line -> !line.isEmpty() && !line.startsWith("#"))
+                .toList();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.index < this.lines.size();
+    }
+
+    @Override
+    public String next() {
+        return this.lines.get(this.index++);
+    }
+
+    public abstract void read();
 }
