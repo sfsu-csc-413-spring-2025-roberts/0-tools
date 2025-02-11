763384ee7b9b50800331c20e71d502d20d4d2774
John Roberts <roberts.john@gmail.com>
2025-02-11 14:21:03 -0800

    added command line args; wip lexer and parser file generation

--- src/tools/CompilerTools.java
+++ src/tools/CompilerTools.java
@@ -1,6 +1,7 @@
 package tools;
 
 import java.nio.file.Path;
+import java.util.Arrays;
 
 import tools.grammar.Grammar;
 import tools.readers.GrammarReader;
@@ -8,14 +9,42 @@ import tools.writers.TextWriter;
 
 public class CompilerTools {
     public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
+        if (args.length == 0 || Arrays.asList(args).contains("-help") || Arrays.asList(args).contains("-h")) {
             System.out.println("Usage: java CompilerTools <grammar-definition-filepath>");
+            System.out.println("Options:");
+            System.out.printf("   %-15s %s%n", "-d --debug",
+                    "Debug mode to output debugging information while reading the grammar");
+            System.out.printf("   %-15s %s%n", "-h --help", "Show this help message");
+            System.out.printf("   %-15s %s%n", "-v --verbose", "Output result of reading the grammar file");
+            System.out.printf("   %-15s %s%n", "-1 --lexer",
+                    "(Re)Generate the files for the lexer (token kind enumeration and symbol table)");
+            System.out.printf("   %-15s %s%n", "-2 --parser",
+                    "(Re)Generate the files for the lexer (ASTs) and basic visitors");
             System.exit(1);
         }
 
-        Grammar grammar = new GrammarReader(Path.of(args[0])).read();
+        boolean debug = hasCommandLineArgument(args, "--debug", "-d");
+        boolean verbose = hasCommandLineArgument(args, "--verbose", "-v");
 
-        TextWriter writer = new TextWriter(grammar);
-        writer.write();
+        boolean parser = hasCommandLineArgument(args, "--parser", "-2");
+        boolean lexer = hasCommandLineArgument(args, "--lexer", "-1") || parser;
+
+        Grammar grammar = new GrammarReader(Path.of(args[0]), debug).read();
+
+        if (verbose) {
+            new TextWriter(grammar).write();
+        }
+
+        if (parser) {
+            System.out.println("Generating parser files...");
+        }
+
+        if (lexer) {
+            System.out.println("Generating lexer files...");
+        }
+    }
+
+    private static boolean hasCommandLineArgument(String[] args, String... options) {
+        return args.length > 1 && Arrays.asList(args).stream().anyMatch(arg -> Arrays.asList(options).contains(arg));
     }
 }
