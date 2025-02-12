0bec517dec681d601ac89c3262b985d8ab829fe5
John Roberts <roberts.john@gmail.com>
2025-02-12 09:08:31 -0800

    minor summary output updates for readability

--- src/tools/writers/SummaryWriter.java
+++ src/tools/writers/SummaryWriter.java
@@ -1,5 +1,7 @@
 package tools.writers;
 
+import java.util.List;
+
 import tools.grammar.Grammar;
 
 public class SummaryWriter extends GrammarWriter {
@@ -15,13 +17,20 @@ public class SummaryWriter extends GrammarWriter {
                 grammar.getNonTerminals().size(), grammar.getAstCount(),
                 grammar.getTerminals().size(), grammar.getProductions().size());
 
-        System.out.println(String.format("Keywords (%d): %s", grammar.getKeywords().size(),
-                String.join(", ", grammar.getKeywords())));
-        System.out.println(String.format("Operators (%d): %s", grammar.getOperators().size(),
-                String.join(", ", grammar.getOperators())));
-        System.out.println(String.format("Separators (%d): %s", grammar.getSeparators().size(),
-                String.join(", ", grammar.getSeparators())));
+        outputLexemes("Keywords", grammar.getKeywords(), 7);
+        outputLexemes("Operators", grammar.getOperators(), 2);
+        outputLexemes("Separators", grammar.getSeparators(), 2);
+
         System.out.println();
     }
 
+    private void outputLexemes(String title, List<String> lexemes, int columnSize) {
+        String lineFormat = "%-20s %s%n";
+        String lexemeFormat = "%-" + columnSize + "s";
+
+        String heading = String.format("%s (%d):", title, lexemes.size());
+        List<String> displayLexemes = lexemes.stream().map(lexeme -> String.format(lexemeFormat, lexeme)).toList();
+
+        System.out.printf(lineFormat, heading, String.join(" ", displayLexemes));
+    }
 }
